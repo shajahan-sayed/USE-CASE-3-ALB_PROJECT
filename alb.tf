251a49d5535b879f7d2d488159ca821ba4e8c457
shajahan-sayed <shajahansayed111@gmail.com>
2025-11-03 12:51:25 +0530

    Create alb.tf

---
+++ alb.tf
@@ -0,0 +1,64 @@
+#creating application load balancer
+resource "aws_lb" "alb_uc3" {
+ name = "alb_uc3"
+ load_balancer_type = "application"
+ security_groups = [aws_security_group.alb_security_group.id]
+
+ subnets = [aws_subnet.subnet_1.id,
+            aws_subnet.subnet_2.id,
+            aws_subnet.subnet_3.id]
+
+ tags = {
+   Name = "alb_uc3"
+ }
+}
+
+#creating target group
+resource "aws_target_group" "tg_home" {
+ name = "tg_home"
+ port = 80
+ protocol = "HTTP"
+ vpc_id = aws_vpc.vpc_alb.id
+
+ tags = {
+  Name = "tg_home"
+ }
+}
+
+resource "aws_target_group" "tg_image" {
+ Name = "tg_image"
+ port = 80
+ protocol = "HTTP"
+ vpc_id = aws_vpc.vpc_alb.id
+
+ tags = {
+  Name = "tg_image"
+ }
+}
+resource "aws_target_group" "tg_register" {
+ name = "tg_register"
+ port = 80
+ protocol = "HTTP"
+ vpc_id = aws_vpc.vpc_alb.id
+
+ tags = {
+  Name = "tg_register"
+ }
+}
+
+#attaching ec2 to target group 
+resource "aws_target_group_attachment" "home" {
+ target_group_arn = aws_target_group.tg_home.arn
+ target_id = aws_instance.web_page1.id
+ port = 80
+}
+resource "aws_target_group_attachment" image" {
+ target_group_arn = aws_target_group.tg_image.arn
+ target_id = aws_instance.web_page2.id
+}
+resource "aws_target_group_attachment" "register" {
+ target_group_arn = aws_target_group.tg_register.arn
+ target_id = aws_instance.web_page3.id
+}
+
+ 

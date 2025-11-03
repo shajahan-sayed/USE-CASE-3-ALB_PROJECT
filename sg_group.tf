75912a343f661f595be1cb40ff712ecce33e697f
shajahan-sayed <shajahansayed111@gmail.com>
2025-11-03 14:10:49 +0530

    Update sg_group.tf

--- sg_group.tf
+++ sg_group.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "alb_security" {
   from_port = 80
   to_port = 80
   protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0]
+  cidr_blocks = ["0.0.0.0/0"]
  }
  ingress { 
   description = "allowing SSH port"
